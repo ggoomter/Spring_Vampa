4d49dd47fc29b7bd0ab3c6805edd06b4e5ae9671
ggoomter <ggoomter@gmail.com>
2022-01-19 23:13:40 +0900

    환경설정 1,2,3 이후 첫페이지 한글성공

---
+++ VamPa_Board(Oracle)/src/test/java/com/vam/persistence/JDBCTest.java
@@ -0,0 +1,35 @@
+package com.vam.persistence;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+
+public class JDBCTest {
+	
+	
+    static { 
+        try { 
+            Class.forName("oracle.jdbc.driver.OracleDriver"); 
+        } catch(Exception e) { 
+            e.printStackTrace(); 
+        } 
+    } 
+    
+    @Test 
+    public void testConnection() { 
+        try(Connection con = DriverManager.getConnection( 
+                "jdbc:oracle:thin:@localhost:1521:orcl", 
+                "ggoomter", 
+                "rmfpdlxm3")){ 
+            System.out.println(con); 
+        } catch (Exception e) { 
+            fail(e.getMessage()); 
+        } 
+    
+    }    
+        
+}
