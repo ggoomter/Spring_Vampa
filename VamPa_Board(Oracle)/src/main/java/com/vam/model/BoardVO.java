bea2d56ffe2485b2bc1ed06ef33241ee46cb590e
ggoomter <ggoomter@gmail.com>
2022-01-20 22:34:07 +0900

    게시판등록구현1

---
+++ VamPa_Board(Oracle)/src/main/java/com/vam/model/BoardVO.java
@@ -0,0 +1,117 @@
+package com.vam.model;
+
+import java.util.Date;
+
+public class BoardVO {
+
+    /* 게시판 번호 */
+    private int bno;
+    
+    /* 게시판 제목 */
+    private String title;
+    
+    /* 게시판 내용 */
+    private String content;
+    
+    /* 게시판 작가 */
+    private String writer;
+    
+    /* 등록 날짜 */
+    private Date regdate;
+    
+    /* 수정 날짜 */
+    private Date updateDate;
+
+	/**
+	 * @return the bno
+	 */
+	public int getBno() {
+		return bno;
+	}
+
+	/**
+	 * @param bno the bno to set
+	 */
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	/**
+	 * @return the content
+	 */
+	public String getContent() {
+		return content;
+	}
+
+	/**
+	 * @param content the content to set
+	 */
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	/**
+	 * @return the writer
+	 */
+	public String getWriter() {
+		return writer;
+	}
+
+	/**
+	 * @param writer the writer to set
+	 */
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	/**
+	 * @return the regdate
+	 */
+	public Date getRegdate() {
+		return regdate;
+	}
+
+	/**
+	 * @param regdate the regdate to set
+	 */
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+
+	/**
+	 * @return the updateDate
+	 */
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+
+	/**
+	 * @param updateDate the updateDate to set
+	 */
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+
+	@Override
+	public String toString() {
+		return "BoardVO [bno=" + bno + ", title=" + title + ", content=" + content + ", writer=" + writer + ", regdate="
+				+ regdate + ", updateDate=" + updateDate + "]";
+	}
+    
+    
+	
+}
